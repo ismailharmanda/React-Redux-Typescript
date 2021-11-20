5680ed1167c575af0ede76f32ac4d66dcd11965b
ismail harmanda <iharmanda@hotmail.com>
2021-11-20 22:29:35 +0300

    Handling Component State

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 interface AppProps {
-  color: string;
+  color?: string;
   fontSize?: string;
 }
 
 const App: React.FC<AppProps> = ({ color, fontSize }) => {
-  return <div style={{ color }}>Hi there</div>;
+  const [counter, setCounter] = useState(0);
+
+  const onIncrement = (): void => {
+    setCounter((prev) => prev + 1);
+  };
+
+  const onDecrement = (): void => {
+    setCounter((prev) => prev - 1);
+  };
+
+  return (
+    <div>
+      <button onClick={onIncrement}>Increment</button>
+      <button onClick={onDecrement}>Decrement</button>
+      {counter}
+    </div>
+  );
 };
 
 export default App;
